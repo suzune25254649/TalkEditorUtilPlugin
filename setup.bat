0846b5c6eaddda9596f503aaa37f33143973aa07
suzune25254649 <38400438+suzune25254649@users.noreply.github.com>
2021-12-24 17:42:16 +0900

    feat: v1.07 - 拡張編集0.93rc1に対応しました。 - キーコンフィグ機能を追加しました。config/keyconfig.txt を編集してください。 - テキストボックス内でTABキーを押すと、多目的スライダーの数値部にフォーカスが移動するようにしました。セリフを入力し終えたらTABを押し、表情設定にスムーズに移ることができます。 - 以下は連携しているTalkEditorUtilのVerUPによる更新です     - VOICEVOXに対応。     - CoeFont.STUDIOに対応。     - 起動時に自動的にバージョンアップチェックを行うようになりました。

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 setlocal enabledelayedexpansion
 echo -----------------------------------------------------------------------
 echo aviutl.exeを、ここにドラッグインしてEnterを押してね♪
@@ -6,82 +6,10 @@ echo -----------------------------------------------------------------------
 set /p PATH_AVIUTL=">> "
 echo.
 
-for %%I IN ( %PATH_AVIUTL% ) do set "EXENAME=%%~nxI"
-for %%I IN ( %PATH_AVIUTL% ) do set "DIRNAME=%%~dpI"
-
-if /i aviutl.exe neq %EXENAME% (
-	echo Error: ドラッグインされたファイルが、aviutl.exeではありません。
-	echo.
-	pause
-	exit /b 1
+call cscript tools\setup.js //NoLogo %PATH_AVIUTL%
+if 0 equ %errorlevel% (
+	echo SUCCEEDED
+	echo セットアップが正常に完了しました！
 )
-
-mkdir download > NUL 2>&1
-echo TalkEditorUtilのダウンロードを開始
-if not exist .\download\TalkEditorUtil_v1.1.3.zip @bitsadmin /transfer TalkEditorUtilのダウンロード /PRIORITY FOREGROUND https://github.com/suzune25254649/TalkEditorUtil/releases/download/v1.1.3/TalkEditorUtil_v1.1.3.zip %~dp0\download\TalkEditorUtil_v1.1.3.zip
-if not exist .\download\TalkEditorUtil_v1.1.3.zip @bitsadmin /transfer TalkEditorUtilのダウンロード /PRIORITY FOREGROUND https://github.com/suzune25254649/TalkEditorUtil/releases/download/v1.1.3/TalkEditorUtil_v1.1.3.zip %~dp0\download\TalkEditorUtil_v1.1.3.zip
-
-if not exist .\download\TalkEditorUtil_v1.1.3.zip (
-	echo ERROR
-	echo ダウンロードに失敗しました。
-	echo 時々失敗することがあるようなので、何度かやってみてください。
-	echo;
-	pause
-	exit /b 1
-)
-if exist .\download\TalkEditorUtil rmdir /s /q .\download\TalkEditorUtil
-powershell -NoProfile -ExecutionPolicy Unrestricted .\tools\unzip.ps1 download\TalkEditorUtil_v1.1.3.zip download
-
-if exist "%DIRNAME%exedit.auf" (
-	echo "%DIRNAME%"にインストールします。
-) else if exist "%DIRNAME%plugins\exedit.auf" (
-	set DIRNAME=%DIRNAME%plugins\
-	echo "!DIRNAME!"にインストールします。
-) else (
-	echo 拡張編集プラグインである exedit.auf が見当たりません。
-	echo 拡張編集0.92を導入してください（0.93は非対応です）
-	echo;
-	pause
-	exit /b 1
-)
-
-mkdir "%DIRNAME%\TalkEditorUtil" > NUL 2>&1
-mkdir "%DIRNAME%\TalkEditorUtil\dropfiles" > NUL 2>&1
-mkdir "config" > NUL 2>&1
-
-set err=0
-
-xcopy /Y /Q tools\aviutl_plugin\TalkEditorUtil.auf "%DIRNAME%"
-if %ERRORLEVEL% neq 0 (set err=1)
-
-xcopy /Y /Q tools\aviutl_plugin\TalkEditorUtil.dll "%DIRNAME%"
-if %ERRORLEVEL% neq 0 (set err=2)
-
-xcopy /Y /Q tools\aviutl_plugin\*.exa "%DIRNAME%\TalkEditorUtil\dropfiles"
-if %ERRORLEVEL% neq 0 (set err=3)
-
-xcopy /Y /E /Q download\TalkEditorUtil\tools\macro\RemoteTalkEditor*.exe "%DIRNAME%\TalkEditorUtil\"
-if %ERRORLEVEL% neq 0 (set err=4)
-
-xcopy /Y /E /Q download\TalkEditorUtil\tools\macro\*.dll "%DIRNAME%\TalkEditorUtil\"
-if %ERRORLEVEL% neq 0 (set err=5)
-
-xcopy /Y /E /Q download\TalkEditorUtil\config\* "config"
-if %ERRORLEVEL% neq 0 (set err=6)
-
-if %err% neq 0 (
-	echo FAILED
-	echo セットアップに失敗しました。
-	echo 下記を対応の後、もう一度お試しください（それでもダメならPC再起動を）
-	echo ・aviutlを全て終了してください。
-	echo ・使っていたVoiceoid2、A.I.VOICE、ガイノイドのエディタを全て閉じてください
-	echo ・RemoteTalkEditor64を終了してください（タスクトレイに残っていませんか？）
-	echo;
-	pause
-	exit /b 1
-)
-
-echo SUCCEEDED
-echo セットアップが正常に完了しました！
-echo;
 pause
+exit /b %errorlevel%
---
+++ tools/setup.js
@@ -0,0 +1,302 @@
+var version = "1.1.4";
+var filenameTalkEditorUtilZip = "v" + version + ".zip";
+var dirnameTalkEditorUtil = "TalkEditorUtil-" + version
+
+if (0 == WScript.Arguments.length)
+{
+	WScript.StdOut.WriteLine("Error: 起動引数が足りません。");
+	WScript.Quit(1);
+}
+
+FS = new ActiveXObject("Scripting.FileSystemObject");
+SH = new ActiveXObject('WScript.Shell');
+
+if (!FS.FolderExists("download"))
+{
+	createFolder("download");
+}
+
+var filepathAviutl = WScript.Arguments(0);
+
+if ("aviutl.exe" != FS.getFileName(filepathAviutl))
+{
+	println("Error: ドラッグインされたファイルが、aviutl.exeではありません。");
+	WScript.Quit(1);
+}
+
+//	インストール先フォルダの決定
+var dirnameInstall = FS.GetParentFolderName(filepathAviutl);
+if (FS.FileExists(dirnameInstall + "\\exedit.auf"))
+{
+	println('"' + dirnameInstall + '"' + "にインストールします。");
+}
+else if (FS.FileExists(dirnameInstall + "\\plugins\\exedit.auf"))
+{
+	dirnameInstall += "\\plugins";
+	println('"' + dirnameInstall + '"' + "にインストールします。");
+}
+else
+{
+	println("Error: 拡張編集プラグインである exedit.auf が見当たりません。");
+	println("       拡張編集0.92を導入してください(0.93は非対応です)");
+	WScript.Quit(1);
+}
+
+
+//	インストール先にある、過去のファイル群を消す
+var deletefilepaths = [
+	"TalkEditorUtil.auf",
+	"TalkEditorUtil.dll",
+	"TalkEditorUtil.exa",
+	"TalkEditorUtil/TalkEditorUtilPlugin.exa",
+	"TalkEditorUtil/dropfiles/TalkEditorUtilPlugin.exa",
+	"TalkEditorUtil/dropfiles/字幕準備.exa",
+	"TalkEditorUtil/dropfiles/統合.exa",
+	"TalkEditorUtil/RemoteTalkEditor32.exe",
+	"TalkEditorUtil/RemoteTalkEditor64.exe",
+	"TalkEditorUtil/Codeer.Friendly.dll",
+	"TalkEditorUtil/Codeer.Friendly.Dynamic.dll",
+	"TalkEditorUtil/Codeer.Friendly.Windows.dll",
+	"TalkEditorUtil/Codeer.Friendly.Windows.Grasp.2.0.dll",
+	"TalkEditorUtil/Codeer.Friendly.Windows.Grasp.3.5.dll",
+	"TalkEditorUtil/Codeer.Friendly.Windows.NativeStandardControls.4.0.dll",
+	"TalkEditorUtil/Codeer.Friendly.Windows.NativeStandardControls.dll",
+	"TalkEditorUtil/Codeer.Friendly.Windows.NativeStandardControls.Generator.dll",
+	"TalkEditorUtil/Codeer.TestAssistant.GeneratorToolKit.dll",
+	"TalkEditorUtil/RM.Friendly.WPFStandardControls.3.0.dll",
+	"TalkEditorUtil/RM.Friendly.WPFStandardControls.3.0.Generator.dll",
+	"TalkEditorUtil/RM.Friendly.WPFStandardControls.3.5.dll",
+	"TalkEditorUtil/RM.Friendly.WPFStandardControls.4.0.dll",
+	"TalkEditorUtil/RM.Friendly.WPFStandardControls.4.0.Generator.dll",
+];
+
+createFolder("download/__temp__");
+if (!cleanup(deletefilepaths, dirnameInstall, "download/__temp__"))
+{
+	//deleteFolder("download/__temp__");
+	println("Error: セットアップに失敗しました。");
+	println("       下記を対応の後、もう一度お試しください(それでもダメならPC再起動を)");
+	println("       ・aviutlを全て終了してください。");
+	println("       ・使っていたVoiceoid2、A.I.VOICE、ガイノイドのエディタを全て閉じてください");
+	println("       ・RemoteTalkEditor64を終了してください(タスクトレイに残っていませんか？)");
+	WScript.Quit(1);
+}
+deleteFolder("download/__temp__");
+
+//	TalkEditorUtilのダウンロードと、解凍
+if (!FS.FileExists("download/" + filenameTalkEditorUtilZip))
+{
+	if (0 != download("https://github.com/suzune25254649/TalkEditorUtil/archive/refs/tags/" + filenameTalkEditorUtilZip, "download/" + filenameTalkEditorUtilZip))
+	{
+		println('Error: "' + filenameTalkEditorUtilZip + '"のダウンロードに失敗しました。');
+		if (FS.FileExists("download/" + filenameTalkEditorUtilZip))
+		{
+			deleteFile("download/" + filenameTalkEditorUtilZip);
+		}
+		WScript.Quit(1);
+	}
+}
+
+deleteFolder("download/" + dirnameTalkEditorUtil);
+var command = "powershell -NoProfile -ExecutionPolicy Unrestricted .\\tools\\unzip.ps1 download\\" + filenameTalkEditorUtilZip + " download";
+SH.Run(command, 10, true);
+
+//	インストールを行う
+try
+{
+	FS.CopyFile("tools/aviutl_plugin/*", dirnameInstall);
+	FS.CopyFolder("tools/aviutl_plugin/*", dirnameInstall);
+	FS.CopyFile("download/" + dirnameTalkEditorUtil + "/tools/macro/RemoteTalkEditor*.exe", dirnameInstall + "/TalkEditorUtil");
+	FS.CopyFile("download/" + dirnameTalkEditorUtil + "/tools/macro/*.dll", dirnameInstall + "/TalkEditorUtil");
+
+	FS.CopyFolder("download/" + dirnameTalkEditorUtil + "/config", "./");
+	FS.CopyFile("tools/config/*", "./config");
+	WScript.Quit(0);
+}
+catch (error)
+{
+	println("Error: セットアップに失敗しました。");
+	WScript.Quit(1);
+}
+
+function print(text)
+{
+	WScript.StdOut.Write(text);
+}
+
+function println(text)
+{
+	WScript.StdOut.WriteLine(text);
+}
+
+function download(url, filenameSave)
+{
+	var command = "bitsadmin /transfer TalkEditorUtilのダウンロード /PRIORITY FOREGROUND " + url + " " + FS.GetAbsolutePathName(filenameSave);
+	return SH.Run(command, 10, true);
+}
+
+function createFolder(folderpath)
+{
+	if (FS.FolderExists(folderpath))
+	{
+		return true;
+	}
+	try
+	{
+		var parents = [];
+		while ("" != folderpath && !FS.FolderExists(folderpath))
+		{
+			parents.push(folderpath);
+			folderpath = FS.GetParentFolderName(folderpath);
+		}
+		
+		parents = parents.reverse();
+		for(var i = 0; i < parents.length; ++i)
+		{
+			FS.CreateFolder(parents[i]);
+		}
+		return true;
+	}
+	catch (error)
+	{
+	}
+	return false;
+}
+
+function copyFolder(from, to)
+{
+	if (!FS.FolderExists(from))
+	{
+		return false;
+	}
+
+	try
+	{
+		FS.CopyFolder(from, to);
+		return true;
+	}
+	catch (error)
+	{
+	}
+	return false;
+}
+
+function deleteFolder(folderpath)
+{
+	if (!FS.FolderExists(folderpath))
+	{
+		return true;
+	}
+	try
+	{
+		FS.DeleteFolder(folderpath);
+		return true;
+	}
+	catch (error)
+	{
+	}
+	return false;
+}
+
+function copyFile(from, to)
+{
+	if (!FS.FileExists(from))
+	{
+		return false;
+	}
+
+	try
+	{
+		createFolder(FS.GetParentFolderName(to));
+		FS.CopyFile(from, to);
+		return true;
+	}
+	catch (error)
+	{
+	}
+	return false;
+}
+
+function moveFile(from, to)
+{
+	if (!FS.FileExists(from))
+	{
+		return false;
+	}
+
+	try
+	{
+		createFolder(FS.GetParentFolderName(to));
+		FS.MoveFile(from, to);
+		return true;
+	}
+	catch (error)
+	{
+	}
+	return false;
+}
+
+function deleteFile(filepath)
+{
+	if (!FS.FileExists(filepath))
+	{
+		return true;
+	}
+
+	try
+	{
+		FS.DeleteFile(filepath);
+		return true;
+	}
+	catch (error)
+	{
+	}
+	return false;
+}
+
+/**
+指定のファイル群の削除を行うが、途中で1ファイルでも失敗した場合は巻き戻す
+*/
+function cleanup(filepaths, from_dirname, temp_dirname)
+{
+	var flg = false;
+	var rollbacks = [];
+	
+	for (var i = 0; i < filepaths.length; i++)
+	{
+		if (undefined == filepaths[i])
+		{
+			continue;
+		}
+		
+		if (!FS.FileExists(from_dirname + "/" + filepaths[i]))
+		{
+			continue;
+		}
+		if (!copyFile(from_dirname + "/" + filepaths[i], temp_dirname + "/" + filepaths[i]))
+		{
+			//FS.DeleteFolder(temp_dirname);
+			println("copyFile:" + from_dirname + "/" + filepaths[i]);
+			return false;
+		}
+		if (!deleteFile(from_dirname + "/" + filepaths[i]))
+		{
+			println("deleteFile:" + i);
+			flg = true;
+			break;
+		}
+		rollbacks.push(filepaths[i]);
+	}
+	
+	if (!flg)
+	{
+		return true;
+	}
+	
+	for (var i = 0; i < rollbacks.length; i++)
+	{
+		moveFile(temp_dirname + "/" + rollbacks[i], from_dirname + "/" + rollbacks[i]);
+	}
+	FS.DeleteFolder(temp_dirname);
+	return false;
+}
